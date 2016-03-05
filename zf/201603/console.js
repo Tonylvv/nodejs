014228817faed41943b48c6cf4525bc6ed1672f9
guohongliang <johnnyss@vip.qq.com>
2016-03-05 11:09:24 +0800

    初始化

---
+++ zf/201603/console.js
@@ -0,0 +1,11 @@
+/**
+ * Created by TonyTed on 16/3/4.
+ */
+
+console.error('err');
+console.warn('warn');
+console.log('log');
+console.info('info');
+console.time('ks');
+for(var i = 0; i < 1000000; i++) {}
+console.timeEnd('ks');
---
+++ zf/201603/hello_word.js
@@ -0,0 +1,6 @@
+/**
+ * Created by TonyTed on 16/3/4.
+ */
+console.log("hello guo hong liang ! Welcome to nodejs ! 2016");
+console.log(__dirname);
+console.log(__filename);
